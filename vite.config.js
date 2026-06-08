12f7e7c38731add66b6d26bd9d3ada9558c4232f
GorillaTV <114786487+GorillaTV@users.noreply.github.com>
2026-06-07 20:55:09 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 // The `base` must match the GitHub Pages project path:
 // https://<user>.github.io/mission-across-the-universe/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/mission-across-the-universe/' : '/',
+  base: '/',
 });
